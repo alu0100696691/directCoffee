4e46d4d9e8451f84efe30b1c2e3801cea8e1a52e
alu0100696691 <alu0100696691@ull.edu.es>
2018-06-03 11:26:30 +0100

    routes, header/menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  {path: '', component: InicioComponent },
+  {path: 'inicio', component: InicioComponent },
+  {path: '**', component: InicioComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
