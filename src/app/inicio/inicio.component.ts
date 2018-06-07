e360dde6ff3dce812f2309042a283a48e709ece3
alu0100696691 <alu0100696691@ull.edu.es>
2018-06-07 17:31:50 +0100

    pantalla inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutentificacionService } from '../servicios/autentificacion.service'; 
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private autentificacionService: AutentificacionService) { }
 
   ngOnInit() {
+  }
+
+  isAuth() {
+    return this.autentificacionService.isAuthenticated();
   }
 
 }
